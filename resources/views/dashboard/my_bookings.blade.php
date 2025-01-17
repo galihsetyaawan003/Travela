76487f5acb95f963f9673a854905f00d18004dd7
galihsetyaawan003 <galihsetyaawan003@gmail.com>
2025-01-17 08:42:29 +0700

    commit 1701

--- resources/views/dashboard/my_bookings.blade.php
+++ resources/views/dashboard/my_bookings.blade.php
@@ -19,7 +19,7 @@
           <p class="font-semibold">My Packages</p>
 
           @forelse(Auth()->user()->bookings as $booking)
-          <a href="trip-details.html" class="card">
+          <a href="{{route('dashboard.booking.details', $booking->id)}}" class="card">
             <div class="bg-white p-4 rounded-[26px] flex items-center gap-4">
               <p class="text-center text-sm leading-[22px] tracking-035">
                 <span class="font-semibold text-2xl">{{$booking->start_date->format('d')}}
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{asset('output.css')}}" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-  <!-- CSS -->
-  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-</head>
-<body class="font-poppins text-black">
+@extends('front.layouts.app')
+@section('content')
+
     <section id="content" class="max-w-[640px] w-full mx-auto bg-[#F9F2EF] min-h-screen flex flex-col gap-8 pb-[120px]">
         <nav class="mt-8 px-4 w-full flex items-center justify-between">
           <div class="flex items-center gap-3">
@@ -210,12 +202,13 @@
           </a>
         </div>
     </section>
+@endsection
 
+
+@push('after-scripts')
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <!-- JavaScript -->
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     <script src="{{asset('js/flickity-slider.js')}}"></script>
     <script src="{{asset('js/two-lines-text.js')}}"></script>
-
-</body>
-</html>
+@endpush
